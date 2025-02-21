30d5f6b7f5b8bee9ae7aaa551d9b57689785d13a
unknown <Artur.Tsorionov@advalange.com>
2025-02-21 20:07:46 +0300

    Init

---
+++ stack.go
@@ -0,0 +1,30 @@
+package stack
+
+import "errors"
+
+type stack[T any] struct {
+	data []T
+}
+
+func MakeStack[T any](size uint) stack[T] {
+	return stack[T]{data: make([]T, size)}
+}
+
+func (s *stack[T]) Push(elems ...T) {
+	s.data = append(s.data, elems...)
+}
+
+func (s *stack[T]) Pop() error {
+	if len(s.data) <= 0 {
+		return errors.New("stack is empty")
+	}
+	s.data = s.data[:len(s.data)-1]
+	return nil
+}
+
+func (s *stack[T]) Top() (*T, error) {
+	if len(s.data) <= 0 {
+		return nil, errors.New("stack is empty")
+	}
+	return &s.data[len(s.data)-1], nil
+}
